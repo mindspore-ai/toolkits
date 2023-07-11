1803b5b8bb77b6d1c761eec25d1b764304648774
l30036321 <lvkaimeng@huawei.com>
2023-07-11 14:47:25 +0800

    add ptdbg tool

---
+++ troubleshooter/troubleshooter/ptdbg_ascend/hook_module/register_hook.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# Copyright (C) 2019-2020. Huawei Technologies Co., Ltd. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+
+import functools
+import os
+
+import torch
+
+from . import wrap_torch, wrap_functional, wrap_tensor, wrap_vf, wrap_module
+from .hook_module import HOOKModule
+from ..common.utils import check_file_or_directory_path, print_error_log, CompareException, Const, \
+    print_info_log, print_warn_log, get_process_rank
+from ..dump.utils import make_dump_dirs
+from ..common import global_manage
+
+try:
+    import torch_npu
+except ImportError:
+    is_gpu = True
+else:
+    is_gpu = False
+    from . import wrap_npu_custom
+
+make_dir_flag = True
+
+global_manage.init()
+
+def initialize_hook(hook):
+    wrap_tensor.wrap_tensor_ops_and_bind(hook)
+    for attr_name in dir(wrap_tensor.HOOKTensor):
+        if attr_name.startswith("wrap_"):
+            setattr(torch.Tensor, attr_name[5:], getattr(wrap_tensor.HOOKTensor, attr_name))
+
+    wrap_torch.wrap_torch_ops_and_bind(hook)
+    for attr_name in dir(wrap_torch.HOOKTorchOP):
+        if attr_name.startswith("wrap_"):
+            setattr(torch, attr_name[5:], getattr(wrap_torch.HOOKTorchOP, attr_name))
+
+    wrap_functional.wrap_functional_ops_and_bind(hook)
+    for attr_name in dir(wrap_functional.HOOKFunctionalOP):
+        if attr_name.startswith("wrap_"):
+            setattr(torch.nn.functional, attr_name[5:], getattr(wrap_functional.HOOKFunctionalOP, attr_name))
+
+    wrap_vf.wrap_vf_ops_and_bind(hook)
+    for attr_name in dir(wrap_vf.HOOKVfOP):
+        if attr_name.startswith("wrap_"):
+            setattr(torch._VF, attr_name[5:], getattr(wrap_vf.HOOKVfOP, attr_name))
+
+    if not is_gpu:
+        wrap_npu_custom.wrap_npu_ops_and_bind(hook)
+        for attr_name in dir(wrap_npu_custom.HOOKNpuOP):
+            if attr_name.startswith("wrap_"):
+                setattr(torch_npu, attr_name[5:], getattr(wrap_npu_custom.HOOKNpuOP, attr_name))
+
+
+def register_hook(model, hook, **kwargs):
+    global make_dir_flag
+    assert hasattr(model, "named_modules"), "Please register hooks to nn.Module."
+    print_info_log("Please disable the shuffle function of the dataset "
+                   "and the dropout function of the model "
+                   "before running the program.")
+    dump_step = kwargs.get('dump_step', 1)
+    overflow_nums = kwargs.get('overflow_nums', 1)
+    dump_mode, dump_config_file = init_dump_config(kwargs)
+
+    pid = os.getpid()
+    rank = kwargs.get('rank')
+    need_clear = True
+    if rank is None:
+        rank, need_clear = get_process_rank(model)
+    if make_dir_flag:
+        make_dump_dirs(rank)
+        make_dir_flag = False
+    hook_name = hook.__name__
+
+    if "overflow_check" in hook_name and not is_gpu:
+        if hasattr(torch_npu._C, "_enable_overflow_npu"):
+            torch_npu._C._enable_overflow_npu()
+            print_info_log("Enable overflow function success.")            
+        else:
+            print_warn_log("Api '_enable_overflow_npu' is not exist, "
+                           "the overflow detection function on milan platform maybe not work! "
+                           "please check the version of software torch_npu.")
+        # In NPU scene, clear the overflow flag before overflow detection
+        if need_clear:
+            torch_npu.npu.set_device(rank)
+            torch_npu._C._clear_overflow_npu()
+
+    print_info_log("Start mounting the {} hook function to the model.".format(hook_name))
+    hook = functools.partial(hook, dump_step=dump_step, overflow_nums=overflow_nums, pid=pid,
+                             dump_mode=dump_mode, dump_config=dump_config_file)
+    print_info_log("The {} hook function is successfully mounted to the model.".format(hook_name))
+
+    initialize_hook(hook)
+
+    for _, module in model.named_modules():
+        if hasattr(module, 'hook_name'):
+            prefix_nn_name_ = "NN_" + str(module.hook_name[5:]) + "_"
+            module.register_forward_hook(hook(prefix_nn_name_ + "{}_" + "forward"))
+            module.register_backward_hook(hook(prefix_nn_name_ + "{}_" + "backward"))
+
+
+def init_dump_config(kwargs):
+    dump_mode = kwargs.get('dump_mode', "api")
+    dump_config = kwargs.get('dump_config')
+    dump_config_file = ''
+    if dump_mode not in Const.SUPPORT_DUMP_MODE:
+        print_error_log("dump_mode only support %s" % Const.SUPPORT_DUMP_MODE)
+        raise CompareException(CompareException.INVALID_PARAM_ERROR)
+    if dump_mode == "acl":
+        if dump_config is None:
+            print_error_log("dump_mode is acl mode, dump_config must be configured.")
+            raise CompareException(CompareException.INVALID_PARAM_ERROR)
+        dump_config_file = os.path.realpath(dump_config)
+        check_file_or_directory_path(dump_config_file)
+        if not dump_config.endswith(".json"):
+            print_error_log("dump_config must be configure json file.")
+            raise CompareException(CompareException.INVALID_PARAM_ERROR)
+    return dump_mode, dump_config_file
+
+
+def enable_ms():
+    global_manage.set_value("dump_module", True)
+    global_manage.set_value("expand", False)
+    wrap_module.wrap_nn_module_and_bind()
+    print_info_log("Enable the dump mode for adapting to MindSpore")
---
+++ troubleshooter/troubleshooter/ptdbg_ascend/hook_module/wrap_module.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# Copyright (C) 2019-2020. Huawei Technologies Co., Ltd. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+import os
+import torch.nn as nn
+import yaml
+from ..common import global_manage
+
+cur_path = os.path.dirname(os.path.realpath(__file__))
+yaml_path = os.path.join(cur_path, "support_wrap_ops.yaml")
+
+with open(yaml_path, "r") as f:
+    WrapModuleOps = yaml.safe_load(f).get("Module")
+
+nn_module = {}
+
+for f in dir(nn):
+    nn_module[f] = getattr(nn, f)
+
+
+def get_nn_module():
+    global WrapModuleOps
+    _all_nn_module = dir(nn)
+    assert set(WrapModuleOps) <= set(_all_nn_module)
+    return WrapModuleOps
+
+
+def call_decorator(cls, name):
+    if not global_manage.get_value("dump_module"):
+        return cls
+
+    original_call = cls.__call__
+    cls.hook_name = "wrap_" + name
+    cls._enable_hook = True
+
+    def new_call(self, *args, **kwargs):
+        changed = False
+        if global_manage.get_value("g_stop_hook"):
+            self._enable_hook = False
+        else:
+            global_manage.set_value("g_stop_hook", True)
+            changed = True
+        result = original_call(self, *args, **kwargs)
+        if changed:
+            global_manage.set_value("g_stop_hook", False)
+        return result
+
+    cls.__call__ = new_call
+    return cls
+
+
+def wrap_nn_module_and_bind():
+    get_nn_module()
+    for name in WrapModuleOps:
+        call_decorator(nn_module[name], name)
+
+
